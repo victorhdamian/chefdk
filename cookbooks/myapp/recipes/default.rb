096647b3a416c40c47c94a5a8e8585c830f9d35f
victorhdamian <victorhdamian@gmail.com>
2015-09-28 07:59:20 -0400

    modified on myapp default.rb

--- cookbooks/myapp/recipes/default.rb
+++ cookbooks/myapp/recipes/default.rb
@@ -4,22 +4,25 @@
 #
 # Copyright (c) 2015 The Authors, All Rights Reserved.
 
-package 'httpd' do
+package 'apache','httpd' do
   action :install
 end
 
-file '/var/www/html/index.html' do
-  action :create
-  content 'Hello people'
+template '/var/www/html/index.html' do
+  source 'index.html.erb'
   owner 'root'
   group 'root'
   mode '0644'
+  variables ({
+  	:platform => node['platform'],
+  	:platform_version => node['platform_version'],
+  	:running_on => node['virtualization']['system'],
+  	:localip => node['ipaddress'],
+  	})
 end
+
 
 service 'httpd' do
   supports :status => true, :restart => true, :reload => true
   action [:start, :enable]
 end
-
-
-
