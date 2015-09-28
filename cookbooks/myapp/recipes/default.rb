d59a27efe220cd8a647b9e109f474abe97d4f971
victorhdamian <victorhdamian@gmail.com>
2015-09-28 07:54:36 -0400

    modified typo on myapp default.rb

--- cookbooks/myapp/recipes/default.rb
+++ cookbooks/myapp/recipes/default.rb
@@ -3,3 +3,23 @@
 # Recipe:: default
 #
 # Copyright (c) 2015 The Authors, All Rights Reserved.
+
+package 'httpd' do
+  action :install
+end
+
+file '/var/www/html/index.html' do
+  action :create
+  content 'Hello people'
+  owner 'root'
+  group 'root'
+  mode '0644'
+end
+
+service 'httpd' do
+  supports :status => true, :restart => true, :reload => true
+  action [:start, :enable]
+end
+
+
+
