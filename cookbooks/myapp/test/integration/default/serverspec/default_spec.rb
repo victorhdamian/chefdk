e7f246fc98b9cf58c0572534e9e06eb8854c7763
victorhdamian <victorhdamian@gmail.com>
2015-09-27 21:12:19 -0400

    modified myapp default_spec.rb

--- cookbooks/myapp/test/integration/default/serverspec/default_spec.rb
+++ cookbooks/myapp/test/integration/default/serverspec/default_spec.rb
@@ -7,3 +7,18 @@ describe 'myapp::default' do
     skip 'Replace this with meaningful tests'
   end
 end
+
+describe package('httpd') do
+  it { should be_installed }
+end
+
+describe service('httpd') do
+  it { should be_running }
+end
+
+describe port(80) do
+	it { should be_listening}
+end
+
+describe file('/var/www/html/index.html') do
+	it { should contain 'Hello'}
